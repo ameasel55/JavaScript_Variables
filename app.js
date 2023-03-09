33bda7997d47f1afb9e784f95b19ecc882d3d335
Adam Measel <ameasel55@gmail.com>
2023-03-09 10:03:21 -0600

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,55 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Steve"
+let lastName = "Guttenberg"
+var age = 64;
+
+console.log (firstName)
+console.log (lastName)
 // Exercise 2
 
+string = firstName
+string = lastName
+Number = age
+
+console.log (age)
 // Exercise 3
+
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitve = true
+
+console.log (language)
+console.log (createdYear)
+console.log (isCaseSensitve)
+
+// Exercise 4
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+console.log (price)
+console.log (isOnSale)
+console.log (salePercentage)
+console.log (stock)
+console.log (inStock)
+console.log (selectedSize)
+// Exercise 5
+
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookMark = 456
+let hasRead = true
+
+console.log (title)
+console.log (author)
+console.log (pageCount)
+console.log (bookMark)
+console.log (hasRead)
+
